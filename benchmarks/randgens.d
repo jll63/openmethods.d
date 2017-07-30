86370546c17287f2efe0b28c04b1586e0e50e4a7
Jean-Louis Leroy <jl@leroy.nyc>
2017-07-30 10:09:50 -0400

    benchmark for random number generators

---
+++ benchmarks/randgens.d
@@ -0,0 +1,24 @@
+/+
+ dub.sdl:
+ name "randgens"
+ targetType "executable"
+ dependency "openmethods" path="../"
+ +/
+
+import std.stdio, std.datetime, std.random, std.meta, std.string;
+
+void main()
+{
+  foreach (r; AliasSeq!(LinearCongruentialEngine, MersenneTwisterEngine, XorshiftEngine)) {
+    auto name = r.stringof[0 .. r.stringof.indexOf('(') ];
+    StopWatch sw;
+    sw.start();
+    foreach (unused; 0..1_000_000_000) {
+
+    }
+    double took = sw.peek.msecs;
+    writefln("%30s: %.0f msecs", name, took);
+
+  }
+
+}
